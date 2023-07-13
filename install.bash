9805bc260c947e78ceb7546d6bfa25342cb9b250
mnfrombd <139509142+mnfrombd@users.noreply.github.com>
2023-07-13 21:35:34 +0000

    Update install.bash

--- install.bash
+++ install.bash
@@ -56,8 +56,8 @@ echo -e "${Green}This process needs super user permission (sudo). So please read
 
 make-sure-system-is-updated
 
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 echo ""
